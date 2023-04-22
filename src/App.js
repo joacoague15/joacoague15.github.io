adec13c1ba3d74f4efb881e9a20833b5aceb3b13
joacoague15 <joacoague15@gmail.com>
2023-04-22 02:03:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: "flex", width: "100vw" }}>
+        <img src="https://media.discordapp.net/attachments/703709686238543946/1095776966025695312/joacoague_Paladin_symmetrical_flat_icon_design_gothic_dark_dd_b_310cff7a-4bd2-4ed6-94e5-8989b17e257f.png" alt="paladin-img" />
+      </div>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <button>Angry</button>
+        <button>Focused</button>
+        <button>Happy</button>
+      </div>
     </div>
   );
 }
